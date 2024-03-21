a2ac1c0e3ea42a9a6462ebaeb406d91c06ab924a
MaximSchoemaker <maxim.schoemaker@gmail.com>
2024-03-21 05:11:12 +0100

    index command

--- packages/@tinacms/cli/src/index.ts
+++ packages/@tinacms/cli/src/index.ts
@@ -2,6 +2,7 @@ import { Cli, Builtins } from 'clipanion'
 //@ts-ignore
 import { version, name } from '../package.json'
 import { DevCommand } from './next/commands/dev-command'
+import { IndexCommand } from './next/commands/index-command'
 import { BuildCommand } from './next/commands/build-command'
 import { AuditCommand } from './next/commands/audit-command'
 import { InitCommand } from './next/commands/init-command'
@@ -22,6 +23,7 @@ const cli = new Cli({
 })
 
 cli.register(DevCommand)
+cli.register(IndexCommand)
 cli.register(BuildCommand)
 cli.register(AuditCommand)
 cli.register(InitCommand)
